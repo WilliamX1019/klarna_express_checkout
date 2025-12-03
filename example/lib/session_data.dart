88f9d20598e486bb0904f2ec5caeff88891b326a
WilliamX <846947300@qq.com>
2025-12-03 11:33:28 +0800

    更新测试 session data

--- example/lib/session_data.dart
+++ example/lib/session_data.dart
@@ -1,19 +1,67 @@
 final sessionData = {
-  "purchase_country": "US",
-  "purchase_currency": "USD",
-  "locale": "en-US",
-  "order_amount": 1000,
-  "order_tax_amount": 0,
-  "order_lines": [
-    {
-      "type": "physical",
-      "reference": "test-item123",
-      "name": "Test Item121",
-      "quantity": 1,
-      "unit_price": 1000,
-      "tax_rate": 0,
-      "total_amount": 1000,
-      "total_tax_amount": 0
-    }
-  ]
+    "acquiring_channel": "ECOMMERCE",
+    "locale": "en-US",
+    "merchant_reference1": "UM883179790889259441",
+    "merchant_urls": {
+        "confirmation": "https://lm.unice.com:18000/order/payWait?pay_method=klarna_payments&order_id=4176443",
+        "push": "https://lapi.unice.com:19000/api/external/klarna/webhook/push?sign=eyJpdiI6InhrSEs0NTVvakFGUm5hNit5OWdrOXc9PSIsInZhbHVlIjoiT2J6YjlWcmpyU2hiU1BHdGRyZE9OUT09IiwibWFjIjoiYmYwYTNlNTc0ZDQxZTYzODFmZWQzYmY0NGE4NjViMzY2MzE4YmU5MzJiMWI0NDdlOTJkY2M0Y2NkNzljY2I3MSIsInRhZyI6IiJ9",
+        "authorization": "https://lapi.unice.com:19000/api/external/klarna/webhook/authorization?sign=eyJpdiI6InhrSEs0NTVvakFGUm5hNit5OWdrOXc9PSIsInZhbHVlIjoiT2J6YjlWcmpyU2hiU1BHdGRyZE9OUT09IiwibWFjIjoiYmYwYTNlNTc0ZDQxZTYzODFmZWQzYmY0NGE4NjViMzY2MzE4YmU5MzJiMWI0NDdlOTJkY2M0Y2NkNzljY2I3MSIsInRhZyI6IiJ9"
+    },
+    "order_amount": 9486,
+    "order_lines": [
+        {
+            "name": "UNice Chestnut Brown Ombre 3 Pcs Body Wave Human Hair Bundles",
+            "quantity": 1,
+            "reference": "3-V-HW-T128-BW-302",
+            "tax_rate": 0,
+            "total_amount": 10986,
+            "total_tax_amount": 0,
+            "type": "physical",
+            "unit_price": 10986,
+            "product_url": "https://lm.unice.com:18000/unice-chestnut-brown-ombre-3-pcs-pack-body-wave-human-hair-bundle.html",
+            "image_url": "https://media.unice.com/ts/media/20240117/e6cccf3d4c373e9f51e4b287081ff228.jpg"
+        },
+        {
+            "name": "Shipping Amount",
+            "quantity": 1,
+            "reference": "shipping_amount",
+            "tax_rate": 0,
+            "total_amount": 0,
+            "total_tax_amount": 0,
+            "type": "shipping_fee",
+            "unit_price": 0
+        },
+        {
+            "name": "Sales Tax",
+            "quantity": 1,
+            "reference": "sales_tax",
+            "tax_rate": 0,
+            "total_amount": 0,
+            "total_tax_amount": 0,
+            "type": "sales_tax",
+            "unit_price": 0
+        },
+        {
+            "name": "Discount",
+            "quantity": 1,
+            "reference": "discount",
+            "tax_rate": 0,
+            "total_amount": -1500,
+            "total_tax_amount": 0,
+            "type": "discount",
+            "unit_price": -1500
+        },
+        {
+            "name": "Surcharge",
+            "quantity": 1,
+            "reference": "surcharge",
+            "tax_rate": 0,
+            "total_amount": 0,
+            "total_tax_amount": 0,
+            "type": "surcharge",
+            "unit_price": 0
+        }
+    ],
+    "purchase_country": "US",
+    "purchase_currency": "USD"
 };
